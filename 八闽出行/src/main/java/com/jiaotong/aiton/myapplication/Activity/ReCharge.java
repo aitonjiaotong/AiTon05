4e6c039484f8f44e0f332366a7e9c3970355accd
zjb <zjb@aiton.com.cn>
2015-12-30 11:19:11 +0800

    2015年12月30日11:10:02 增加了充值界面

--- 八闽出行/src/main/java/com/jiaotong/aiton/myapplication/Activity/ReCharge.java
+++ 八闽出行/src/main/java/com/jiaotong/aiton/myapplication/Activity/ReCharge.java
@@ -1,9 +1,7 @@
 package com.jiaotong.aiton.myapplication.Activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 
 import com.jiaotong.aiton.myapplication.R;
 
@@ -15,25 +13,4 @@ public class ReCharge extends AppCompatActivity {
         setContentView(R.layout.activity_re_charge);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_re_charge, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
